f5b118ca9300f3fa75112a2dd99a45419bd81c9d
Xiaoyu Lu <xlu14@slb.com>
2020-11-17 16:41:42 +0800

    feat: add computation module and test cases

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -7,7 +7,8 @@ export class MultiplicationTable {
     }
     else
     {
-      return '1*1=1'
+      const expressions = buildExpressions(start, end)
+      return render(expressions)
     }
   }
 }
@@ -17,5 +18,49 @@ function isValidInput(start: number, end: number): boolean
   return start <= 10 && start >= 1 && end <= 10 && end >= 1 && start <= end
 }
 
-
-// function buildExpressions(start: number, end: number):
+class Expressions{
+  factor1: number
+  factor2: number
+  product: number
+  constructor(factor1: number, factor2: number, product: number){
+    this.factor1 = factor1
+    this.factor2 = factor2
+    this.product = product
+  }
+}
+function buildExpressions(start: number, end: number): Expressions[][]
+{
+  const result : Expressions[][] = []
+  console.log('building expressions')
+  for(let i = 0; i <= end - start; i++)
+  {
+    result[i] = []
+    for(let k = 0; k <= i; k++)
+    {
+      //console.log("i = "+ i +" k = " + k)
+      const expression = new Expressions(start + k, start + i, (start + i)*(start + k))
+      //console.log('result[' + (i-start) + "][" + (j-start)+"] is "+ result[i-start][j-start].product)
+      result[i].push(expression)
+    }
+  }
+  return result
+}
+function render(expressions: Expressions[][]): string
+{
+  let result = ''
+  for(let i = 0; i < expressions.length; i++)
+  {
+    for(let k = 0; k <= i; k++)
+    {
+      //console.log("adding expressions[" + i + "][" + k + "]")
+      result += expressions[i][k].factor1 + '*' + expressions[i][k].factor2 + '=' + expressions[i][k].product + '  '
+      if(k === i && k !== expressions.length - 1)
+      {
+        result = result.trimEnd()
+        result += '\n'
+      }
+    }
+  }
+  result = result.trimEnd()
+  return result
+}
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -42,4 +42,16 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('')
   })
+  it('should render multiplication table of (2, 4)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 4
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('2*2=4\n2*3=6  3*3=9\n2*4=8  3*4=12  4*4=16')
+  })
 })
