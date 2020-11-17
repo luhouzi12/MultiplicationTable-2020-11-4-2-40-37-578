e96cf7b9c259bc5621202576e4c12c37b13edf89
Xiaoyu Lu <xlu14@slb.com>
2020-11-17 14:43:22 +0800

    feat: add isValidRange

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,21 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    //const result = ''
+    if(!isValidInput(start, end))
+    {
+      return ''
+    }
+    else
+    {
+      return '1*1=1'
+    }
   }
 }
+
+function isValidInput(start: number, end: number): boolean
+{
+  return start <= 10 && start >= 1 && end <= 10 && end >= 1 && start <= end
+}
+
+
+// function buildExpressions(start: number, end: number):
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,6 +1,9 @@
 import {MultiplicationTable} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
+  // beforeEach(() => {
+  //   const table = new MultiplicationTable()
+  // })
   it('should render multiplication table of (1, 1)', () => {
     // given
     const table = new MultiplicationTable()
@@ -12,5 +15,18 @@ describe('Multiplication Table', () => {
 
     //then
     expect(rendered).toBe('1*1=1')
+  })
+
+  it('start should not be greater than end', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
   })
 })
